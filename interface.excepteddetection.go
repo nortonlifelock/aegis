64c9671f6cb1434379c45c33d6c5774e6fd4ee42
ryan.everhart <ryan_everhart@symantec.com>
2020-02-19 13:30:14 -0700

    adding new interface to tickets with exceptions

---
+++ interface.excepteddetection.go
@@ -0,0 +1,16 @@
+package domain
+
+import "time"
+
+type ExceptedDetection interface {
+	Title() string
+	IP() string
+	Hostname() string
+	VulnerabilityID() string
+	VulnerabilityTitle() string
+	Approval() *string
+	Expires() *time.Time
+	AssignmentGroup() string
+	OS() string
+	IgnoreID() string
+}
