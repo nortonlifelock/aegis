04ce10a687e6affa66c33207bcc318b955a0ff0d
ryan.everhart <ryan_everhart@symantec.com>
2020-02-27 15:15:18 -0700

    adding the ordefault methods to the single file as installation is causing problems on windows

--- tools/jira-export/jira_export.go
+++ tools/jira-export/jira_export.go
@@ -215,3 +215,27 @@ func dump(path string, lineOut string) {
 
 	mut.Unlock()
 }
+
+func tord(in *time.Time) (out time.Time) {
+	if in != nil {
+		out = *in
+	}
+
+	return out
+}
+
+func sord(in *string) (out string) {
+	if in != nil {
+		out = *in
+	}
+
+	return out
+}
+
+func ford(in *float32) (out float32) {
+	if in != nil {
+		out = *in
+	}
+
+	return out
+}
