628427dfd7091b8ce0101b97537400f3609a6dfb
ryan.everhart <ryan_everhart@symantec.com>
2020-03-03 14:50:03 -0700

    now using normal terminal if password terminal fails

--- tools/jira-export/jira_export.go
+++ tools/jira-export/jira_export.go
@@ -57,104 +57,105 @@ func main() {
 				var password []byte
 				fmt.Println("Enter JIRA Password: ")
 
-				if password, err = terminal.ReadPassword(int(os.Stdin.Fd())); err == nil {
+				if password, err = terminal.ReadPassword(int(os.Stdin.Fd())); err != nil {
+					reader := bufio.NewReader(os.Stdin)
+					text, _ := reader.ReadString('\n')
+					password = []byte(text)
+				}
 
-					var tickets *jira.ConnectorJira
-					if tickets, err = jira.ConnectJira(*apiPath, *user, string(password), exportLogger{}); err == nil {
+				var tickets *jira.ConnectorJira
+				if tickets, err = jira.ConnectJira(*apiPath, *user, string(password), exportLogger{}); err == nil {
 
-						var JQL string
+					var JQL string
 
-						reader := bufio.NewReader(os.Stdin)
-						fmt.Print("Enter JQL: ")
-						if JQL, err = reader.ReadString('\n'); err == nil {
-							JQL = strings.TrimSpace(JQL)
+					reader := bufio.NewReader(os.Stdin)
+					fmt.Print("Enter JQL: ")
+					if JQL, err = reader.ReadString('\n'); err == nil {
+						JQL = strings.TrimSpace(JQL)
 
-							// TODO: Remove this once the closed-error status is part of exceptions
-							//statuses["Closed-Error"] = true
-							var wg = sync.WaitGroup{}
-							var sm = sync.Map{}
+						// TODO: Remove this once the closed-error status is part of exceptions
+						//statuses["Closed-Error"] = true
+						var wg = sync.WaitGroup{}
+						var sm = sync.Map{}
 
-							var tix = tickets.GetByCustomJQLChan(JQL)
+						var tix = tickets.GetByCustomJQLChan(JQL)
 
-							dump(outputFile, fmt.Sprintf("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%v,%s,%s,%s,%s,%s,%s,%s,%s\n",
-								"KEY",
-								"SUMMARY",
-								"ASSIGNMENTGROUP",
-								"STATUS",
-								"METHODOFDISCOVERY",
-								"ORG",
-								"CREATED",
-								"ALERTDATE",
-								"DUE",
-								"RESOLUTIONDATE",
-								"PRIORITY",
-								"CVSS",
-								"CERF",
-								"PORTS",
-								"OS",
-								"HOSTNAME",
-								"IPADDRESS",
-								"DEVICEID",
-								"VULNERABILITY",
-								"VULNERABILITYID"))
+						dump(outputFile, fmt.Sprintf("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%v,%s,%s,%s,%s,%s,%s,%s,%s\n",
+							"KEY",
+							"SUMMARY",
+							"ASSIGNMENTGROUP",
+							"STATUS",
+							"METHODOFDISCOVERY",
+							"ORG",
+							"CREATED",
+							"ALERTDATE",
+							"DUE",
+							"RESOLUTIONDATE",
+							"PRIORITY",
+							"CVSS",
+							"CERF",
+							"PORTS",
+							"OS",
+							"HOSTNAME",
+							"IPADDRESS",
+							"DEVICEID",
+							"VULNERABILITY",
+							"VULNERABILITYID"))
 
-							var tickets = make([]domain.Ticket, 0)
-							var ticketLock = &sync.Mutex{}
+						var tickets = make([]domain.Ticket, 0)
+						var ticketLock = &sync.Mutex{}
 
-							for {
-								if issue, ok := <-tix; ok {
-									wg.Add(1)
+						for {
+							if issue, ok := <-tix; ok {
+								wg.Add(1)
 
-									go func(ticket domain.Ticket) {
-										defer wg.Done()
-										saveUniqueTicket(ticket, &sm, ticketLock, &tickets)
-									}(issue)
+								go func(ticket domain.Ticket) {
+									defer wg.Done()
+									saveUniqueTicket(ticket, &sm, ticketLock, &tickets)
+								}(issue)
 
-								} else {
-									break
-								}
-							}
-
-							wg.Wait()
-
-							fmt.Printf("Writing %d tickets\n", len(tickets))
-
-							for index := range tickets {
-								var ticket = tickets[index]
-
-								var resolved string
-								if ticket.ResolutionDate() != nil {
-									resolved = tord(ticket.ResolutionDate()).Format(csvDate)
-								}
-
-								dump(outputFile, fmt.Sprintf("%s,\"%s\",%s,%s,%s,%s,%s,%s,%s,%s,%s,%v,%s,%s,%s,\"%s\",%s,%s,\"%s\",%v\n",
-									ticket.Title(),
-									sord(ticket.Summary()),
-									sord(ticket.AssignmentGroup()),
-									sord(ticket.Status()),
-									sord(ticket.MethodOfDiscovery()),
-									sord(ticket.OrgCode()),
-									tord(ticket.CreatedDate()).Format(csvDate),
-									tord(ticket.AlertDate()).Format(csvDate),
-									tord(ticket.DueDate()).Format(csvDate),
-									resolved,
-									sord(ticket.Priority()),
-									ford(ticket.CVSS()),
-									ticket.CERF(),
-									sord(ticket.ServicePorts()),
-									sord(ticket.OperatingSystem()),
-									sord(ticket.HostName()),
-									sord(ticket.IPAddress()),
-									ticket.DeviceID(),
-									sord(ticket.VulnerabilityTitle()),
-									ticket.VulnerabilityID()))
+							} else {
+								break
 							}
 						}
-					} else {
-						fmt.Printf("Error opening JIRA connection - check your username and password [%s]\n", err.Error())
+
+						wg.Wait()
+
+						fmt.Printf("Writing %d tickets\n", len(tickets))
+
+						for index := range tickets {
+							var ticket = tickets[index]
+
+							var resolved string
+							if ticket.ResolutionDate() != nil {
+								resolved = tord(ticket.ResolutionDate()).Format(csvDate)
+							}
+
+							dump(outputFile, fmt.Sprintf("%s,\"%s\",%s,%s,%s,%s,%s,%s,%s,%s,%s,%v,%s,%s,%s,\"%s\",%s,%s,\"%s\",%v\n",
+								ticket.Title(),
+								sord(ticket.Summary()),
+								sord(ticket.AssignmentGroup()),
+								sord(ticket.Status()),
+								sord(ticket.MethodOfDiscovery()),
+								sord(ticket.OrgCode()),
+								tord(ticket.CreatedDate()).Format(csvDate),
+								tord(ticket.AlertDate()).Format(csvDate),
+								tord(ticket.DueDate()).Format(csvDate),
+								resolved,
+								sord(ticket.Priority()),
+								ford(ticket.CVSS()),
+								ticket.CERF(),
+								sord(ticket.ServicePorts()),
+								sord(ticket.OperatingSystem()),
+								sord(ticket.HostName()),
+								sord(ticket.IPAddress()),
+								ticket.DeviceID(),
+								sord(ticket.VulnerabilityTitle()),
+								ticket.VulnerabilityID()))
+						}
 					}
 				} else {
-					fmt.Println("Error while reading password from terminal")
+					fmt.Printf("Error opening JIRA connection - check your username and password [%s]\n", err.Error())
 				}
 			} else {
 				fmt.Println("Username (-user) is required")
