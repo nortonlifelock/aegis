85332e4dccbda663b8dbdc56c1ab0ce719a471b4
ryan.everhart <ryan_everhart@symantec.com>
2020-03-12 10:16:58 -0700

    removing part of a migration that may not work for all

--- init/migrations/1583338631_adding_table_for_assignment_rules.up.sql
+++ init/migrations/1583338631_adding_table_for_assignment_rules.up.sql
@@ -13,18 +13,5 @@ CREATE TABLE AssignmentRules(
     CONSTRAINT `fk_vm_ar_tk` FOREIGN KEY (`TagKeyID`) REFERENCES `TagKey` (`Id`)
 );
 
-UPDATE Job SET SourceTypeIn = '2', SourceTypeOut = '1' WHERE ID = '1';
-UPDATE Job SET SourceTypeIn = '1', SourceTypeOut = '2' WHERE ID = '2';
-UPDATE Job SET SourceTypeIn = '2', SourceTypeOut = '1' WHERE ID = '3';
-UPDATE Job SET SourceTypeIn = '2', SourceTypeOut = '1' WHERE ID = '4';
-UPDATE Job SET SourceTypeIn = '1', SourceTypeOut = '1' WHERE ID = '5';
-UPDATE Job SET SourceTypeIn = '1', SourceTypeOut = '2' WHERE ID = '6';
-UPDATE Job SET SourceTypeIn = '2', SourceTypeOut = '2' WHERE ID = '7';
-UPDATE Job SET SourceTypeIn = '1', SourceTypeOut = '1' WHERE ID = '8';
-UPDATE Job SET SourceTypeIn = '3', SourceTypeOut = '3' WHERE ID = '9';
-UPDATE Job SET SourceTypeIn = '1', SourceTypeOut = '1' WHERE ID = '10';
-UPDATE Job SET SourceTypeIn = '4', SourceTypeOut = '2' WHERE ID = '11';
-UPDATE Job SET SourceTypeIn = '2', SourceTypeOut = '2' WHERE ID = '12';
-
 ALTER TABLE AssignmentRules DROP COLUMN VulnTitleSubstring;
 ALTER TABLE AssignmentRules CHANGE `TagKeyValue` `TagKeyRegex` VARCHAR(100) NULL;
