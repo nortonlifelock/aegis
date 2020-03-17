78d6fe82dbb7a148352f23a4c89504cee2c83004
ryan.everhart <ryan_everhart@symantec.com>
2020-03-17 15:50:48 -0700

    adding the last ticket date to the asset group interface

--- interface.assetgroup.go
+++ interface.assetgroup.go
@@ -1,8 +1,11 @@
 package domain
 
+import "time"
+
 // AssetGroup defines the interface
 type AssetGroup interface {
 	GroupID() int
 	ScannerSourceID() string
 	CloudSourceID() *string
+	LastTicketing() *time.Time
 }
