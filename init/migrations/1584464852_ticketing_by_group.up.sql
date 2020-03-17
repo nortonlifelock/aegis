4282a4fd039acc7925e7252d2924b4ec0d3f0e8c
ryan.everhart <ryan_everhart@symantec.com>
2020-03-17 11:41:48 -0700

    Revert "new sprocs for ISSUE4"
    This reverts commit 5e58c6c0e00445bf1cbcbf8dbeb3abd4e1c270f9.

--- init/migrations/1584464852_ticketing_by_group.up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE AssetGroup ADD COLUMN LastTicketing DATETIME NULL AFTER CloudSourceID;
-
-UPDATE AssetGroup AG SET AG.LastTicketing = (SELECT LastJobStart from JobConfig JC WHERE JC.OrganizationID = AG.OrganizationID AND JC.JobID = (SELECT ID FROM Job WHERE Struct = 'TicketingJob') LIMIT 1);
--- init/procedures/AssetGroup/UpdateAssetGroupLastTicket.sql
+++
@@ -1,5 +0,0 @@
-DROP PROCEDURE IF EXISTS `UpdateAssetGroupLastTicket`;
-
-CREATE PROCEDURE `UpdateAssetGroupLastTicket` (inGroupID VARCHAR(36), inOrgID VARCHAR(36), inLastTicketTime DATETIME)
-    #BEGIN#
-UPDATE AssetGroup AG SET AG.LastTicketing = inLastTicketTime WHERE AG.OrganizationID = inOrgID AND AG.GroupID = inGroupID;
