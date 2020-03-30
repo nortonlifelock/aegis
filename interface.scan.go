88e843858023a9e9086ebbc619e2794af885678d
ryan.everhart <ryan_everhart@symantec.com>
2020-03-30 10:40:15 -0700

    adding scan title to interface

--- interface.scan.go
+++ interface.scan.go
@@ -3,5 +3,6 @@ package domain
 // Scan is an interface that captures important information regarding the scan that is created inside of a scanner
 type Scan interface {
 	ID() string
+	Title() string
 	Status() (string, error)
 }
