baa8b4ff352c01ae808d5b1c07580a7a8980eddb
ryan.everhart <ryan_everhart@symantec.com>
2020-06-02 10:31:40 -0700

    interface update

--- generated.interface.go
+++ generated.interface.go
@@ -71,7 +71,7 @@ type GeneratedDatabaseConnection interface {
 	GetDetectionInfoAfter(_After time.Time, _OrgID string) ([]DetectionInfo, error)
 	GetDetectionInfoByID(_ID string) (DetectionInfo, error)
 	GetDetectionInfoBySourceVulnID(_SourceDeviceID string, _SourceVulnerabilityID string, _Port int, _Protocol string) (DetectionInfo, error)
-	GetDetectionInfoForGroupAfter(_After time.Time, _OrgID string, inGroupID string) ([]DetectionInfo, error)
+	GetDetectionInfoForGroupAfter(_After time.Time, _OrgID string, inGroupID string, replaceThisLater bool) ([]DetectionInfo, error)
 	GetDetectionStatusByID(_ID int) (DetectionStatus, error)
 	GetDetectionStatusByName(_Name string) (DetectionStatus, error)
 	GetDetectionStatuses() ([]DetectionStatus, error)
