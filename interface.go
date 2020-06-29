3d86c25688190b6477b7f67fb9d02f4524b2aa05
ryan.everhart <ryan_everhart@symantec.com>
2020-06-29 13:33:20 -0700

    adding ability to update device id

--- interface.go
+++ interface.go
@@ -161,6 +161,14 @@ func (connector *ConnectorJira) UpdateTicket(ticket domain.Ticket, comment strin
 				}
 			}
 
+			if len(ticket.DeviceID()) > 0 {
+				field := connector.GetFieldMap(backendDeviceID)
+				if field != nil {
+					updateBlock.Fields.DeviceID = ticket.DeviceID()
+					oldToNewFieldName["deviceid"] = field.ID
+				}
+			}
+
 			if len(sord(ticket.SystemName())) > 0 {
 				field := connector.GetFieldMap(backendSystemName)
 				if field != nil {
