d5ab883390e719cf1093581bc17efba4fab4bfa8
ryan.everhart <ryan_everhart@symantec.com>
2020-07-23 10:48:19 -0700

    debug message

--- sns.go
+++ sns.go
@@ -76,6 +76,7 @@ func (client *SNSClient) sendMessages() {
 		for {
 			select {
 			case message := <-client.readMessages:
+				fmt.Printf("read %s\n", message)
 				messages = append(messages, message)
 			default:
 				return
@@ -104,10 +105,12 @@ func (client *SNSClient) PushMessage(message string) {
 		// the timeout is twice as long as the waiting period between SNS bursts
 		tic := time.Tick(2 * snsWaitTime)
 
+		fmt.Printf("trying to write %s\n", message)
 		select {
 		case <-tic:
 			return
 		case client.writeMessages <- message:
+			fmt.Printf("wrote %s\n", message)
 		}
 	}()
 }
