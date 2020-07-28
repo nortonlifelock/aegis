721e3cf4233770093424635552c3ecd34a1784ba
ryan.everhart <ryan_everhart@symantec.com>
2020-07-28 10:24:31 -0700

    increasing scan pause to 30 minutes

--- internal/implementations/rescan.go
+++ internal/implementations/rescan.go
@@ -83,8 +83,8 @@ func (job *RescanJob) Process(ctx context.Context, id string, appconfig domain.C
 					for i := 0; i < len(tickets); i += batchSize {
 						scansCreated++
 						if scansCreated%20 == 0 {
-							job.lstream.Send(log.Infof("created 20 scans, waiting 15 minutes before continuing with next batches"))
-							time.Sleep(time.Minute * 15)
+							job.lstream.Send(log.Infof("created 20 scans, waiting 30 minutes before continuing with next batches"))
+							time.Sleep(time.Minute * 30)
 							job.lstream.Send(log.Infof("finished waiting"))
 						}
 
