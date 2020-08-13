46ffdaaf9912977d9babb2ca11f43e85a90a6803
ryan.everhart <ryan_everhart@symantec.com>
2020-08-13 11:39:11 -0700

    duplicate variable declaration

--- pkg/aqua/exception.go
+++ pkg/aqua/exception.go
@@ -60,7 +60,6 @@ func (cli *APIClient) CreateException(finding domain.ImageFinding, comment strin
 
 			var request *http.Request
 			if request, err = http.NewRequest(http.MethodPost, fmt.Sprintf("%s/%s", cli.baseURL, endpoint), bytes.NewReader(body)); err == nil {
-				var body []byte
 				if body, err = cli.executeRequest(request); err == nil {
 				} else {
 					err = fmt.Errorf("error while creating image scan - %s", err.Error())
