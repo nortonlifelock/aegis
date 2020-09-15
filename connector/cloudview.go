a1954636751e4bc2a414d394da1f2d88708d5241
ryan.everhart <ryan_everhart@symantec.com>
2020-09-15 10:18:26 -0700

    returning findings

--- connector/cloudview.go
+++ connector/cloudview.go
@@ -3,6 +3,6 @@ package connector
 import "github.com/nortonlifelock/domain"
 
 func (session *QsSession) RescanBundle(bundleID int, cloudAccountID string) (findings []domain.Finding, err error) {
-	err = session.apiSession.GetCloudViewFindings(cloudAccountID)
-	return nil, err
+	findings, err = session.apiSession.GetCloudViewFindings(cloudAccountID)
+	return findings, err
 }
