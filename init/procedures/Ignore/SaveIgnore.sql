f206974b4ad580592620e83e8a06bf56eb4d34ef
ryan.everhart <ryan_everhart@symantec.com>
2021-01-14 13:33:26 -0700

    allowing the same ignore entry to exist under multiple orgs

--- init/procedures/Ignore/SaveIgnore.sql
+++ init/procedures/Ignore/SaveIgnore.sql
@@ -2,8 +2,8 @@ DROP PROCEDURE IF EXISTS `SaveIgnore`;
 
 CREATE PROCEDURE `SaveIgnore` (_SourceID NVARCHAR(36), _OrganizationID NVARCHAR(36), _TypeID INT, _VulnerabilityID NVARCHAR(120), _DeviceID NVARCHAR(360), _DueDate DATETIME, _Approval NVARCHAR(120), _Active BIT, _port NVARCHAR(150))
   #BEGIN#
-    IF EXISTS (SELECT * FROM `Ignore` WHERE DeviceID = _DeviceID AND VulnerabilityID = _VulnerabilityID AND Port = _Port) THEN
-        UPDATE `Ignore` SET TypeID = _TypeID, DueDate = _DueDate, Approval = _Approval, Active = _Active, DBUpdatedDate = NOW() WHERE DeviceID = _DeviceID AND VulnerabilityID = _VulnerabilityID AND Port = _Port;
+    IF EXISTS (SELECT * FROM `Ignore` WHERE DeviceID = _DeviceID AND VulnerabilityID = _VulnerabilityID AND Port = _Port and OrganizationID = _OrganizationID) THEN
+        UPDATE `Ignore` SET TypeID = _TypeID, DueDate = _DueDate, Approval = _Approval, Active = _Active, DBUpdatedDate = NOW() WHERE DeviceID = _DeviceID AND VulnerabilityID = _VulnerabilityID AND Port = _Port and OrganizationID = _OrganizationID;
     ELSE
         INSERT INTO `Ignore` (SourceID, OrganizationID, TypeID, VulnerabilityID, DeviceID, DueDate, Approval, Active, Port)
             VALUE (_SourceID, _OrganizationID, _TypeID, _VulnerabilityID, _DeviceID, _DueDate, _Approval, _Active, _port);
